59f11d1f83558d577006c7b23011e6ec18e71abd
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-11-09 03:16:01 -0600

    Project has been updated

---
+++ lib/fancy_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class FancyButton extends StatelessWidget {
+  FancyButton({@required this.onPressed});
+
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
